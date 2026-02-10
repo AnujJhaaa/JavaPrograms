af9473721648e225788259eefc093b4603fb7187
Anuj Jha <anuj.jha18.05@gmail.com>
2026-02-10 20:31:08 +0530

    Upload 10.02.2026 : Using IDE EclipseJee 2025-09

---
+++ EclipseJee25-09/java-app-10022026/src/com/codegnan/javaapp10022026/shapes/Shape.java
@@ -0,0 +1,5 @@
+package com.codegnan.javaapp10022026.shapes;
+
+public interface Shape {
+	public abstract void displayInfo();
+}
---
+++ EclipseJee25-09/java-app-10022026/src/com/codegnan/javaapp10022026/shapes/threedshape/ThreeDShapes.java
@@ -0,0 +1,9 @@
+package com.codegnan.javaapp10022026.shapes.threedshape;
+
+import com.codegnan.javaapp10022026.shapes.Shape;
+
+public abstract class ThreeDShapes implements Shape {
+	public abstract int volume();
+	public abstract int surfaceArea();
+}
+
